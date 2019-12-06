7a7db04f6f98e9506721d0b3aeffe451c88fc3f6
Andrew Maddocks <andrewmadd@icloud.com>
2019-12-05 17:35:54 -0700

    finished MVP

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,6 +1,27 @@
 import React from "react";
-import * as rtl from "@testing-library/react";
+import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-
+import App, { strike } from "../App";
 import Display from "./Display";
 import Dashboard from "./Dashboard";
+import { testNameToKey } from "jest-snapshot/build/utils";
+
+test("renders dashboard component", () => {
+  const wrapper = render(<Dashboard />);
+  const element = wrapper.getByText(/hit/i);
+  expect(element).toBeInTheDocument();
+});
+test("App renders without crashing", () => {
+  render(<App />);
+});
+test("App renders without crashing", () => {
+  render(<Display />);
+});
+test("App renders without crashing", () => {
+  render(<Dashboard />);
+  // console.log(component);
+});
+
+// test("strike adds 1 to strike", () => {
+//   expect(strike(1)).toBe(2);
+// });
